c2d18d68cd18d027a561871544ee6f1340c5995a
Gilroy <glsio1234@gmail.com>
2025-02-04 15:21:12 +0800

    added test for generating pair

---
+++ Multi Module Maven/my-app/service/src/test/java/com/mycompany/app/PairServicesTest.java
@@ -0,0 +1,34 @@
+package com.mycompany.app;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Random;
+import java.util.ArrayList;
+
+public class PairServicesTest {
+
+    PairServices ps;
+
+    @BeforeEach
+    void setUp(){
+        ps = new PairServices();
+    }
+
+    @Test
+    public void generateTest(){
+        Random rand = new Random();
+        ArrayList<ArrayList<Pair>> list = ps.generate(3, 4, rand);
+        assertEquals(list.size(), 3);
+        assertEquals(list.get(0).size(), 4);
+    }
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+
+}
